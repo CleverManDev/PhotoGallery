93a9d5d237560b2bc03a4a170c26d56a8904f50e
clever_man <clever.man.dev@gmail.com>
2017-02-06 17:54:08 +0300

    Chapter 27 - Broadcast Intents - Using receivers

--- app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -1,16 +1,17 @@
-package com.bignerdranch.android.photogallery.StartupReceiver;
+package com.bignerdranch.android.photogallery;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class java extends BroadcastReceiver {
+public class StartupReceiver extends BroadcastReceiver {
 
 	private static final String TAG = "StartupReceiver";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.i(TAG, "onReceive: " + intent.getAction());
+		boolean isOn = QueryPreferences.
 	}
 }
